3bc91d37c07ee7d98edf4c699909c23475f732f6
Agnilo <agne.butkute99@gmail.com>
2023-12-11 20:41:59 +0200

    tables

---
+++ database/migrations/create_employee_benefits_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeBenefitsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('employee_benefits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+            $table->string('benefit_name');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('employee_benefits');
+    }
+}
+
