1c2b8e7ff5b6de85153250e317e52734cfc89c33
Agnilo <agne.butkute99@gmail.com>
2023-12-11 21:27:54 +0200

    Update 2023_12_11_192201_create_role_user_table.php

--- database/migrations/2023_12_11_192201_create_role_user_table.php
+++ database/migrations/2023_12_11_192201_create_role_user_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('role_id')->unsigned();
-            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
