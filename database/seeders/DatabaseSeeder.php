fe5eb60ad89571004d703766982d8a33aec52ae7
Agnilo <agne.butkute99@gmail.com>
2023-12-13 19:37:30 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+
         $this->call([
             RolesTableSeeder::class,
             UserTableSeeder::class
         ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
