eac5d1a8d17dea842bbb240b06100782c060f888
Agnilo <agne.butkute99@gmail.com>
2023-12-13 18:31:54 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RolesTableSeeder::class);
-        $this->call(UserTableSeeder::class);
+        $this->call([
+            RolesTableSeeder::class,
+            UserTableSeeder::class
+        ]);
     }
 }
