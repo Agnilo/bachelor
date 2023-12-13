5a9794086c959bebf00397352964aefd61510f55
Agnilo <agne.butkute99@gmail.com>
2023-12-13 19:42:44 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Schema::disableForeignKeyConstraints();
 
 
         $this->call([
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             UserTableSeeder::class
         ]);
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        Schema::enableForeignKeyConstraints();
+        
     }
 }
